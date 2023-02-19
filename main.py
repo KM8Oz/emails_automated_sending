62937a4b99a7be95d359b8e925f9401a68c7a45e
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-02-20 00:21:18 +0100

    tested version

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import os
 import argparse
 import smtplib
 import time
+import ssl
 import configparser
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 # Read the email configuration file
 config = configparser.ConfigParser()
@@ -15,6 +19,7 @@ sender_password = config.get('EMAIL', 'SENDER_PASSWORD')
 # Set the email subject and message
 email_subject =  config.get('EMAIL', 'EMAIL_SUBJECT')
 email_message =  config.get('EMAIL', 'EMAIL_MESSAGE')
+file_path =  config.get('EMAIL', 'FILE_PATH')
 
 # Set the SMTP server and port for Gmail
 smtp_server = config.get('EMAIL', 'EMAIL_SERVER')
@@ -28,7 +33,7 @@ args = parser.parse_args()
 # Print authentication message
 print('''
         To send email using your Gmail account, you need to create an app password. \n
-       Follow these steps to create and use an app password: \n
+        Follow these steps to create and use an app password: \n
         1. Go to your Google Account.\n
         2. Select Security.\n
         3. Under "Signing in to Google," select App Passwords. You may need to sign in.\n
@@ -39,12 +44,36 @@ print('''
         ''')
 
 # Log in to the Gmail SMTP server
-server = smtplib.SMTP(smtp_server, smtp_port)
-server.starttls()
-server.login(sender_email, sender_password)
+def send_email(subject, body, file_path=None):
+    message = MIMEMultipart()
+
+    # Set up message headers
+    message["From"] = sender_email
+    message["To"] = args.recipient_email
+    message["Subject"] = subject
+
+    # Add message body
+    message.attach(MIMEText(body, "plain"))
+
+    # Add attachment file
+    if file_path is not None:
+        with open(file_path, "rb") as attachment:
+            file_name = os.path.basename(file_path)
+            part = MIMEApplication(attachment.read(), Name=file_name)
+            part["Content-Disposition"] = f'attachment; filename="{file_name}"'
+            message.attach(part)
+
+    # Create secure SSL context and start server
+    context = ssl.create_default_context()
+    # with smtplib.SMTP(smtp_server, smtp_port, context=context) as server:
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, args.recipient_email, message.as_string())
 
 # Send the email every time_interval minutes
 while True:
-    server.sendmail(sender_email, args.recipient_email, f'Subject: {email_subject}\n\n{email_message}')
+    # server.sendmail(sender_email, args.recipient_email, f'Subject: {email_subject}\n\n{email_message}')
+    send_email(email_subject, email_message, file_path)
     print(f'Sent email to {args.recipient_email}.')
     time.sleep(args.time_interval * 60) # Convert time_interval from minutes to seconds
